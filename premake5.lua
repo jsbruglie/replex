cb179fff263eddcc687a835f4dd896ac1226c6fc
Joao Borrego <joao.borrego@cern.ch>
2020-07-14 13:38:38 +0200

    Add file structure; Add premake config

---
+++ premake5.lua
@@ -0,0 +1,35 @@
+workspace "RePlex"
+  configurations {"Debug", "Release"}
+  -- Use C++ as the target language for all builds
+  language "C++"
+  targetdir "bin/%{cfg.buildcfg}"
+
+  -- Get that C++14 goodness
+  flags { "C++14" }
+
+  filter "configurations:Debug"
+    -- Add the preprocessor definition DEBUG to debug builds
+    defines { "DEBUG" }
+    -- Ensure symbols are bundled with debug builds
+    flags { "Symbols" }
+
+  filter "configurations:Release"
+    -- Add the preprocessor definition RELEASE to debug builds
+    defines { "RELEASE" }
+    -- Turn on compiler optimizations for release builds
+    optimize "On"
+
+  -- RePlex Library
+  project "RePlex"
+    kind "SharedLib"
+    -- recursively glob .h and .cpp files in the lib directory
+    files { "lib/**.h", "lib/**.cpp" }
+
+  -- RePlex Runtime
+  project "RePlexRuntime"
+    kind "ConsoleApp"
+    -- recursively glob .h and .cpp files in the runtime directory
+    files { "runtime/**.h", "runtime/**.cpp" }
+    -- link the RePlexLib library at runtime
+    links { "RePlex" }
+    includedirs { "lib/pub" }
